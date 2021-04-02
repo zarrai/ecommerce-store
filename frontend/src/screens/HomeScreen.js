8583cab167f56ecaa199c88f58c0c55fd65fe35e
SAIF <zarraisaifedine@gmail.com>
2021-04-03 01:42:39 +0200

    manage state with redux

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,16 +1,16 @@
-import React, {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom'
 import axios from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
+import { listProducts } from '../actions/productActions';
 const HomeScreen = (props) => {
 
-  const [products, setProduct] = useState([]);  
-
+  const productList = useSelector(state => state.productList);
+  const { products, loading, error } = productList;
+  const dispatch = useDispatch();
   useEffect(() => {
-    const fetchData = async () => {
-      const { data } = await axios.get("/api/products");
-      setProduct(data);
-    }
-    fetchData();
+    dispatch(listProducts());
+
     return () => {
       //
     };
@@ -18,28 +18,30 @@ const HomeScreen = (props) => {
 
     return (
         <>
-<ul className="products">
-    {
-      products.map(product =>
-        <li key={product._id}>
-          <div className="product">
-            <Link to={'/product/' + product._id}>
-              <img className="product-image" src={product.image} alt="product" />
+      {loading ? <div>Loading...</div> :
+    error ? <div>{error}</div> :
+      <ul className="products">
+        {
+          products.map(product =>
+            <li key={product._id}>
+              <div className="product">
+                <Link to={'/product/' + product._id}>
+                  <img className="product-image" src={product.image} alt="product" />
 
-            </Link>
-            <div className="product-name">
-              <Link to={'/product/' + product._id}>{product.name}</Link>
-            </div>
-            <div className="product-brand">{product.brand}</div>
-            <div className="product-price">${product.price}</div>
-            <div className="product-rating">{product.rating} Stars ({product.numReiews} Reviews)</div>
-          </div>
-        </li>)
-    }
+                </Link>
+                <div className="product-name">
+                  <Link to={'/product/' + product._id}>{product.name}</Link>
+                </div>
+                <div className="product-brand">{product.brand}</div>
+                <div className="product-price">${product.price}</div>
+                <div className="product-rating">{product.rating} Stars ({product.numReiews} Reviews)</div>
+              </div>
+            </li>)
+        }
 
 
 
-  </ul>
+      </ul>}
         </>
     )
 }
