66f470d13207c6827e4134abda76fd072a28aefa
SAIF <zarraisaifedine@gmail.com>
2021-04-03 00:02:54 +0200

    link backend with frontend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom'
-import data from '../data';
+import axios from 'axios';
 const HomeScreen = (props) => {
+
+  const [products, setProduct] = useState([]);  
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get("/api/products");
+      setProduct(data);
+    }
+    fetchData();
+    return () => {
+      //
+    };
+  }, [])
+
     return (
         <>
 <ul className="products">
     {
-      data.products.map(product =>
-        <li>
+      products.map(product =>
+        <li key={product._id}>
           <div className="product">
             <Link to={'/product/' + product._id}>
               <img className="product-image" src={product.image} alt="product" />
