076028a5c4006cc696e811f003c1da81134331eb
SAIF <zarraisaifedine@gmail.com>
2021-04-05 00:38:25 +0200

    implement authentication

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,4 @@
 export default {
-    MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://saif711:zarrai711@ecommerce-app.xxkpw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://saif711:zarrai711@ecommerce-app.xxkpw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret'
 }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,14 @@ import {BrowserRouter, Route, Link} from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import SigninScreen from './screens/SigninScreen';
+import { useSelector } from 'react-redux';
+import RegisterScreen from './screens/RegisterScreen';
 
 function App() {
+
+  const userSignin = useSelector(state => state.userSignin);
+  const { userInfo } = userSignin;
 
   const openMenu = () => {
     document.querySelector('.sidebar').classList.add('open');
@@ -15,47 +21,53 @@ function App() {
   };
   return (
     <BrowserRouter>
-    <div className="grid-container">
-    <header className="header">
-      <div className="brand">
-        <button onClick={openMenu}>
-          &#9776;
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>
+              &#9776;
         </button>
-        <Link to="/">Ecommerce Store</Link>
-      </div>
-      <div className="header-links">
-        <a href="cart.html">Cart</a>
-        <a href="signin.html">Sign In</a>
-      </div>
-    </header>
-    <aside className="sidebar">
-      <h3>Shopping Categories</h3>
-      <button className="sidebar-close-button" onClick={closeMenu}>x</button>
-      <ul>
-        <li>
-          <a href="index.html">Pants</a>
-        </li>
+            <Link to="/" >Ecommerce Store</Link>
+          </div>
+          <div className="header-links">
+            <Link to="/cart">Cart</Link>
+            {
+              userInfo ? <Link to="/profile">{userInfo.name}</Link> :
+                <Link to="/signin">Sign In</Link>
+            }
+          </div>
+        </header>
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+          <ul>
+            <li>
+              <a href="index.html">Pants</a>
+            </li>
 
-        <li>
-          <a href="index.html">Shirts</a>
-        </li>
+            <li>
+              <a href="index.html">Shirts</a>
+            </li>
 
-      </ul>
-    </aside>
-    <main className="main">
-      <div className="content">
-        <Route path="/product/:id" component={ProductScreen}/>
-        <Route path="/cart/:id?" component={CartScreen} />
-        <Route path="/" exact={true} component={HomeScreen}/>
+          </ul>
+        </aside>
+        <main className="main">
+          <div className="content">
+            <Route path="/signin" component={SigninScreen} />
+            <Route path="/register" component={RegisterScreen} />
+            <Route path="/product/:id" component={ProductScreen} />
+            <Route path="/cart/:id?" component={CartScreen} />
+            <Route path="/" exact={true} component={HomeScreen} />
 
-      </div>
 
-    </main>
-    <footer className="footer">
-      All right reserved.
+          </div>
+
+        </main>
+        <footer className="footer">
+          All right reserved.
     </footer>
-  </div>
-  </BrowserRouter>
+      </div>
+    </BrowserRouter>
   );
 }
 
