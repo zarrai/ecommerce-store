bc1e75f682faa411ca93269962eaa86ef6c4f4cd
SAIF <zarraisaifedine@gmail.com>
2021-04-15 03:56:14 +0200

    fix build

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,9 @@ const path = require('path');
 const app = express();
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/dist')))
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'))
+        res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'))
     })
 }
 
