f8635e5b581ad1a15e09bb3875d10cfc94583e08
SAIF <zarraisaifedine@gmail.com>
2021-04-15 02:04:17 +0200

    last

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,4 @@
 import express from 'express';
-import data from './data';
-import dotenv from 'dotenv';
 import config from './config';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
@@ -8,7 +6,7 @@ import userRoute from './routes/userRoute';
 import productRoute from './routes/productRoute';
 import orderRoute from './routes/orderRoute';
 
-dotenv.config();
+
 
 const mongodbUrl = config.MONGODB_URL;
 mongoose.connect(mongodbUrl, {
