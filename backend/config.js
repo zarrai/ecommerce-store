870a75bcdcbb9149e53827a4044064da15fc00dd
SAIF <zarraisaifedine@gmail.com>
2021-04-15 03:16:08 +0200

    m

--- backend/config.js
+++ backend/config.js
@@ -5,5 +5,6 @@ dotenv.config();
 export default {
     MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://saif711:zarrai711@ecommerce-app.xxkpw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
-    PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'test'
+    PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'test',
+    PORT: process.env.PORT || '1598'
 }
