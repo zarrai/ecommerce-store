a8605ebdf26e1d7b423d8470f2e386f4b1e77973
SAIF <zarraisaifedine@gmail.com>
2021-04-15 02:04:17 +0200

    last

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export default {
     MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://saif711:zarrai711@ecommerce-app.xxkpw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
