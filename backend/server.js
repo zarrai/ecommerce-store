0c4a88e5b4815174eff582c34466019759c6826e
SAIF <zarraisaifedine@gmail.com>
2021-04-27 03:56:28 +0200

    ggg

--- backend/server.js
+++ backend/server.js
@@ -46,4 +46,4 @@ app.get("/api/config/paypal", (req, res) => {
 //   res.send(data.products);
 // });
 
-app.listen(process.env.PORT, () => { console.log(`Server started at http://localhost:${process.env.PORT}`) });
+app.listen(process.env.PORT, () => { console.log(`db:${process.env.MONGODB_URL}Server started at http://localhost:${process.env.PORT}`) });
